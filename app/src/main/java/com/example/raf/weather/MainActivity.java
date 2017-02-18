c4d6cba5e0d0c7ef502f90158e8f510dbea6212f
Raf <rkrassowski@gmail.com>
2017-02-18 20:16:30 +0000

    location fully working

--- app/src/main/java/com/example/raf/weather/MainActivity.java
+++ app/src/main/java/com/example/raf/weather/MainActivity.java
@@ -34,12 +34,18 @@ public class MainActivity extends AppCompatActivity {
     String icon;
     String summary;
     String currentLocation;
+    String encodedUrl;
+    String queryString;
+
     TextView temperatureText;
     TextView weatherDescription;
+
     ImageView weatherIcon;
 
     LocationManager locationManager;
     LocationListener locationListener;
+
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -55,98 +61,71 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void getLocation() {
 
-        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+    public void encodeUrl(){
 
-        locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
+            String url = "https://api.darksky.net/forecast/";
+            String key = "4a3412e7dc871ea6610d108d3d0c6058/";
+            String location = currentLocation;
+            String parameters = "?exclude=minutely,hourly,alerts,flags?&&units=uk2";
+            queryString = url + key + location + parameters ;
 
-                Log.i("Location", String.valueOf(location.getLatitude()) + ", " + String.valueOf(location.getLongitude()));
-                currentLocation = String.valueOf(location.getLatitude()) + ", " + String.valueOf(location.getLongitude());
-
-
-            }
-
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
-            }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
-
-        if (Build.VERSION.SDK_INT < 23) {
-
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-
-        } else if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-            //get explicit permission
-
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-
-        } else {
-
-
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
-
-        }
+        Log.i("generated url", queryString);
 
     }
 
 
     public void refreshWeather(View view){
 
-        getLocation();
+        encodeUrl();
         updateWeather();
+
     }
 
     public void updateWeather(){
 
-        DownloadTask task = new DownloadTask();
-        JSONObject jsonObject = null;
-        try {
-            jsonObject = task.execute("https://api.darksky.net/forecast/4a3412e7dc871ea6610d108d3d0c6058/51.544524, -0.133146?exclude=minutely,hourly,alerts,flags?&&units=uk2").get();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
+        if (queryString == null){
+
+            temperatureText.setText("Please update weather");
+
+        }else {
+
+            DownloadTask task = new DownloadTask();
+            JSONObject jsonObject = null;
+
+            try {
+                jsonObject = task.execute(queryString).get();
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+
+            Log.i("jo", jsonObject.toString());
+
+            JSONObject currently = null;
+            try {
+                currently = jsonObject.getJSONObject("currently");
+
+                temperature = currently.getString("temperature");
+
+                icon = currently.getString("icon");
+
+                summary = currently.getString("summary");
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            String iconReadable = icon.replace("-", "_");
+
+            Context c = getApplicationContext();
+            int id = c.getResources().getIdentifier("drawable/" + iconReadable, null, c.getPackageName());
+
+            temperatureText.setText(temperature + " \u00B0C");
+            weatherDescription.setText(summary);
+            weatherIcon.setImageResource(id);
         }
-
-        Log.i("jo", jsonObject.toString());
-
-        JSONObject currently = null;
-        try {
-            currently = jsonObject.getJSONObject("currently");
-
-            temperature = currently.getString("temperature");
-
-            icon = currently.getString("icon");
-
-            summary = currently.getString("summary");
-
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        String iconReadable = icon.replace("-", "_");
-
-        Context c = getApplicationContext();
-        int id = c.getResources().getIdentifier("drawable/"+iconReadable, null, c.getPackageName());
-
-        temperatureText.setText(temperature + " \u00B0C");
-        weatherDescription.setText(summary);
-        weatherIcon.setImageResource(id);
     }
 
     public class DownloadTask extends AsyncTask<String, Void, JSONObject> {
@@ -205,7 +184,6 @@ public class MainActivity extends AppCompatActivity {
                 return null;
             }
 
-
         }
     }
 
@@ -218,8 +196,56 @@ public class MainActivity extends AppCompatActivity {
         weatherDescription = (TextView)findViewById(R.id.weatherDescriptionTextView);
         weatherIcon = (ImageView)findViewById(R.id.weatherIcon);
 
-        getLocation();
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                Log.i("Location", String.valueOf(location.getLatitude()) + ", " + String.valueOf(location.getLongitude()));
+                currentLocation = String.valueOf(location.getLatitude()) + ", " + String.valueOf(location.getLongitude());
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if (Build.VERSION.SDK_INT < 23) {
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+        } else if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            //get explicit permission
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+
+        } else {
+
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+
+        }
+
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        currentLocation = String.valueOf(location.getLatitude()) + ", " + String.valueOf(location.getLongitude());
+
+        encodeUrl();
         updateWeather();
     }
 
